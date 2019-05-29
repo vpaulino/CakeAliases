19bf8f770d1633f9226b9420b05b1639f377fdba
Vitor Paulino <vitor.paulino.ext@siemens.com>
2019-05-29 15:33:01 +0100

    first aliases created to support semver version for nuget packages rules

---
+++ src/CakeAliases.BranchesSemVer/GitBranchesSemVer.cs
@@ -0,0 +1,53 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CakeAliases.BranchesSemVer
+{
+    public static class CompleteSemVerGenerator
+    {
+        [CakeMethodAlias]
+        public static string GetVersion(this ICakeContext context, string semverVersion, string branchName = "Main", string buildNumber = null)
+        {
+            string semVerRegEx = "((?:0|[1-9]\\d*)\\.(?:0|[1-9]\\d*)\\.(?:0|[1-9]\\d*))";
+            string alphaSemVerPattern = "((?:0\\d*)\\.(?:0|[1-9]\\d*)\\.(?:0|[1-9]\\d*))";
+
+            if (!Regex.IsMatch(semverVersion, semVerRegEx))
+                throw new ArgumentException("must have a semver version number");
+
+            if (string.IsNullOrEmpty(buildNumber))
+                buildNumber = string.Empty;
+
+           
+            List<string> mainBranches = new List<string>() { "Main", "master" };
+            List<string> developBranches = new List<string>() { "develop", "dev" };
+            List<string> release = new List<string>() { "release","staging","qa" };
+
+            var match = Regex.Match(semverVersion, alphaSemVerPattern);
+            if (match.Success)
+            {
+                return string.Concat(semverVersion, $"-alpha{buildNumber}");
+            }
+
+            if (mainBranches.Contains(branchName))
+            {
+                return semverVersion;
+            }
+
+            if (developBranches.Contains(branchName))
+            {
+                return string.Concat(semverVersion,$"-beta{buildNumber}");
+            }
+
+            if (release.Contains(branchName))
+            {
+                return string.Concat(semverVersion, $"-rc{buildNumber}");
+            }
+
+            return string.Concat(semverVersion, $"-alpha{buildNumber}");
+
+        }
+    }
+}
---
+++ tests/CakeAliases.BranchesSemVer.Tests/CompleteSemVerGeneratorTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Xunit;
+
+namespace CakeAliases.BranchesSemVer.Tests
+{
+    public class CompleteSemVerGeneratorTests
+    {
+        [Fact]
+        public void IfVersionLessThenMajor1_thenAlphaSuffixIsAdded()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "0.1.0");
+
+            Assert.Equal("0.1.0-alpha", version);
+        }
+
+        [Fact]
+        public void IfVersionEqualOfHigherThen1_ThenNoSuffixIsAdded()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "1.0.0");
+
+            Assert.Equal("1.0.0", version);
+
+        }
+
+
+        [Fact]
+        public void IfVersionEqualOfHigherThen1_AndBranchIsRelease_ThenReleaseSuffixMustAppear()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "1.0.0","release");
+
+            Assert.Equal("1.0.0-rc", version);
+
+        }
+
+        [Fact]
+        public void IfVersionEqualOfHigherThen1_AndBranchIsDev_ThenReleaseSuffixMustAppear()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "1.0.0", "dev");
+
+            Assert.Equal("1.0.0-beta", version);
+
+        }
+
+        [Fact]
+        public void IfVersionEqualOfHigherThen1_AndBranchIsNotKnown_ThenReleaseSuffixMustAppear()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "1.0.0", "feature");
+
+            Assert.Equal("1.0.0-alpha", version);
+
+        }
+
+        [Fact]
+        public void IfVersionEqualOfHigherThen1_AndBranchIsNotKnown_AndBuildNumberProvided_ThenReleaseSuffixMustAppear_WithBuildNumber()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "1.0.0", "feature","1");
+
+            Assert.Equal("1.0.0-alpha1", version);
+
+        }
+
+        [Fact]
+        public void IfVersionEqualOfHigherThen1_AndBranchIsDev_AndBuildNumberProvided_ThenReleaseSuffixMustAppear_WithBuildNumber()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "1.0.0", "dev", "1");
+            Assert.Equal("1.0.0-beta1", version);
+        }
+
+        [Fact]
+        public void IfVersionEqualOfHigherThen1_AndBranchIsMaster_AndBuildNumberProvided_ThenReleaseSuffixMustNotAppear()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "1.0.0", "master", "1");
+
+            Assert.Equal("1.0.0", version);
+
+        }
+
+        [Fact]
+        public void IfVersionEqualOfHigherThen1_AndBranchIsRelease_AndBuildNumberProvided_ThenReleaseSuffixMustAppear_WithBuildNumber()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "1.0.0", "release", "1");
+
+            Assert.Equal("1.0.0-rc1", version);
+
+        }
+
+        [Fact]
+        public void IfVersionEqualOfHigherThen1_AndBranchIsMain_AndBuildNumberProvided_ThenReleaseSuffixMustNotAppear()
+        {
+            var version = GitBranchesSemVer.GetVersion(null, "1.0.0", "Main", "1");
+
+            Assert.Equal("1.0.0", version);
+
+        }
+
+
+
+    }
+}
